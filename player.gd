d22d6b81c36a43fd09381c46b8849df12748e306
Memo <pradoguillermojr@gmail.com>
2025-10-16 19:30:59 -0700

    The startings of a player character

---
+++ player.gd
@@ -0,0 +1,23 @@
+extends Node2D
+var currentHP : int = 80
+var maxHP : int = 100
+
+var characterName : String = "Elliot"
+
+var currentEnergy : int = 80
+var maxEnergy : int = 100
+
+var handSize : int = 5 #default amount of cards we can hold on our turn 
+
+var positon : int = 0 #will always stay 0,
+#var satusList = [] #we don't have out statusEffect class done yet, so leaving this here.
+#var sprite : Sprite = $Sprite
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
