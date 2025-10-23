447b15b74fcd5315c94c210eb6752a1028ee440b
Memo <pradoguillermojr@gmail.com>
2025-10-23 14:50:43 -0700

    Updated player, added some tests (?)

--- test.gd
+++ test.gd
@@ -1,1 +1,11 @@
 extends Node
+
+func _init():
+	pass
+	
+# Called when the node enters the scene tree for the FIRST time.
+func _ready() -> void:
+	pass
+	
+func _process(delta: float) -> void:
+	pass
