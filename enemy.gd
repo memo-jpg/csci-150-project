29f990bc72945c2fb74874cfefe7ff44ffce4219
Calvin Fern <calvinafern@gmail.com>
2025-10-23 18:01:44 -0700

    Added additional skeleton to Enemy

--- enemy.gd
+++ enemy.gd
@@ -2,7 +2,12 @@ class_name Enemy extends Node2D
 var currentHp: int
 @export var maxHp: int
 var pos: int
+var currentAction: int
+var actions: action
 var statusEffect
+
+class action:
+	var type
 
 func setHp(newHp):
 	if(newHp<maxHp): {
