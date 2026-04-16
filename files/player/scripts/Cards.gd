b4f853bf5ac16ccc3534894249de0ece0f7cafde
Nestor Garcia <nestorisok@gmail.com>
2026-04-16 12:44:12 -0700

    Majority working, text color legible, need to make adjustments on display location for the cards

--- files/player/scripts/Cards.gd
+++ files/player/scripts/Cards.gd
@@ -22,19 +22,29 @@ func set_index(i: int):
 func update_visuals():
 	if data == null:
 		return
-
+	
 	nameLabel.text = data.name
+	nameLabel.add_theme_color_override("font_color", Color.GOLD)
+	nameLabel.add_theme_font_size_override("font_size", 32)
+	
 	costLabel.text = str(data.energy)
-
+	costLabel.add_theme_color_override("font_color", Color.GOLD)
+	costLabel.add_theme_font_size_override("font_size", 32)
+	
+	
 	if data.type == "attack":
 		descLabel.text = "Deal %d damage" % data.damage
 	elif data.type == "defense":
 		descLabel.text = "Gain %d block" % data.shield
 	else:
 		descLabel.text = data.description
-
+	
+	descLabel.add_theme_color_override("font_color", Color.BLACK)
+	descLabel.add_theme_font_size_override("font_size", 32)
+	
 	if data.sprite != "":
 		spriteNode.texture = load(data.sprite)
+	
 
 
 func _on_area_2d_input_event(_viewport, event, shape_idx):
